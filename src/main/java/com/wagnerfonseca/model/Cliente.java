18898a90ff7bc737a33e6117fa93ea480a38403f
Wagner Fonseca <wagner.fonseca@luizalabs.com>
2019-04-22 10:24:01 -0300

    Como agrupar os dados por uma determinada informação

---
+++ src/main/java/com/wagnerfonseca/model/Cliente.java
@@ -0,0 +1,96 @@
+package com.wagnerfonseca.model;
+
+public class Cliente {
+	
+	private String nome;
+	private String cidade;
+	private String estado;
+	private int idade;
+			
+	public Cliente() {
+	}
+	
+	public Cliente(String nome, String cidade, String estado, int idade) {
+		super();
+		this.nome = nome;
+		this.cidade = cidade;
+		this.estado = estado;
+		this.idade = idade;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public int diferencaIdade(final Cliente outro) {
+		return this.idade - outro.getIdade();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cidade == null) ? 0 : cidade.hashCode());
+		result = prime * result + ((estado == null) ? 0 : estado.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (cidade == null) {
+			if (other.cidade != null)
+				return false;
+		} else if (!cidade.equals(other.cidade))
+			return false;
+		if (estado == null) {
+			if (other.estado != null)
+				return false;
+		} else if (!estado.equals(other.estado))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "nome=" + nome;
+	}
+
+		
+
+}
